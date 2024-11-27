5f0f1507eeb6b024a5d36ff6217e62f676533194
xrl111 <buiduclan000@gmail.com>
2024-11-27 22:10:05 +0700

    update landingpage

--- kaos-sport/src/pages/LandingPage.tsx
+++ kaos-sport/src/pages/LandingPage.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
-}
+const LandingPage: React.FC = () => {
+  return <div>LandingPage</div>;
+};
 
-export default LandingPage
+export default LandingPage;
